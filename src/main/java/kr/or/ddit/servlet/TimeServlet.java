b2548d709dd44b23876a0c41e861cd97056241fa
JoongSeok <diat1450@gmail.com>
2019-05-21 12:28:35 +0900

    jsp commit

---
+++ src/main/java/kr/or/ddit/servlet/TimeServlet.java
@@ -0,0 +1,54 @@
+package kr.or.ddit.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TimeServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		PrintWriter pw = resp.getWriter();
+		
+		// localhost/jsp/timeTable?param=6
+		String param = req.getParameter("i");
+		String param2 = req.getParameter("j");
+		
+		System.out.println("param : " + param);
+		
+		pw.write("<html>");
+		pw.write("	<head>");
+		pw.write("		<title> TimeServlet </title>");
+		pw.write("	<style>");
+		pw.write("		td{border : 1px solid black;}");
+		pw.write("	</style>");
+		pw.write("	</head>");
+		pw.write("	</head>");
+		pw.write("	<body>");
+		pw.write("<table border='1'>");
+//		for (int gop = 1; gop <= 9; gop++) {
+//			pw.write("<tr>");
+//			for (int dan = 2; dan <= 9; dan++) {
+//				pw.write("<td>" + dan + " * " + gop + " = " +dan*gop+ "</td>");
+//			}
+//			pw.write("</tr>");
+//		}
+		for (int i = 1; i <= Integer.parseInt(param2); i++) {
+			pw.write("<tr>");
+			for (int j = 2; j <= Integer.parseInt(param); j++) {
+				pw.write("<td>" + j + " * " + i + " = " + j * i + "</td>");
+			}
+			pw.write("</tr>");
+		}
+		pw.write("</table>");
+		pw.write("	</body>");
+		pw.write("</html>");
+		
+		pw.close();
+	}
+}
